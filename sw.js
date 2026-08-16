568c0ecd26154523c5905ba28569ab34abd72f31
WXX-12352 <2912304429@qq.com>
2026-08-16 15:58:24 +0800

    fix PWA install: sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "ielts-v2";
-const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
+const CACHE = "ielts-v3";
+const ASSETS = ["./manifest.json", "./icon-192.png", "./icon-512.png"];
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
 });
